7cbb7e5c0fa2b8f19c7fcd9037e92d17f9ce51be
zap-rowsdower <brock.whitbread@gmail.com>
2014-04-24 19:20:54 -0400

    completed a few more exercises of jquery.

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -5,99 +5,121 @@
 // Change the background color of '#target' by script. 
 // Hint: you can use .css
 
-$(document).ready(function() {
-  $('#target').css({
-    'background-color': 'red'
-  });
-})
+// $(document).ready(function() {
+//   $('#target').css({
+//     'background-color': 'red'
+//   });
+// })
 
 // EXERCISE: Change Parent
 
 // Change the text in the span, a child of "#target"
 // Hint: you can use .text
 
-$(document).ready(function() {
-  $('span').text("This is new Text");
-})
+// $(document).ready(function() {
+//   $('span').text("This is new Text");
+// })
 
 // EXERCISE: Create Clone
 
 // Create a clone of the span in "#target" and position it under the origin.
 // Hint: you can use .clone and .insertAfter
 
-$(document).ready(function() {
-  $('span').clone().insertAfter('span');
-})
+// $(document).ready(function() {
+//   $('span').clone().insertAfter('span');
+// })
 
 // EXERCISE: Use Filter
 
 // Change background color of the second ".target"
 // Hint: you can use .eq and .css
 
-$(document).ready(function() {
-  $('.target').eq(1).css('background-color', 'black');
-})
+// $(document).ready(function() {
+//   $('.target').eq(1).css('background-color', 'black');
+// })
 
 // EXERCISE: Disable Buttons
 
 // Disable the button
 // Hint: you will have to select the button and can use .attr
 
-$(document).ready(function() {
-  $('button').attr('disabled', true);
-})
+// $(document).ready(function() {
+//   $('button').attr('disabled', true);
+// })
 
 // EXERCISE: Uncheck CheckBoxes
 
 // Uncheck all checkboxes using jQuery
 // Hint: you will have to select the input and can use .removeAttr
 
-$(document).ready(function() {
-  $('input').removeAttr('checked', 'uncheck');
-})
+// $(document).ready(function() {
+//   $('input').removeAttr('checked', 'uncheck');
+// })
 
 // EXERCISE: Change Parent
 
 // Move "#child" from "#parent1" to "#parent2"
 // Hint: you can use .appendTo
 
-$(document).ready(function() {
-  $('#child').appendTo('#parent2');
-})
+// $(document).ready(function() {
+//   $('#child').appendTo('#parent2');
+// })
 
 // EXERCISE: Select A Option in A Select Box
 
 // Select the second option in the selectbox by script
 // Hint: you will have to select the option and can use .eq and .attr
 
-$(document).ready(function() {
-  $('option').eq(1).attr('selected', true);
-})
+// $(document).ready(function() {
+//   $('option').eq(1).attr('selected', true);
+// })
 
 // EXERCISE: Change Size
 
 // Make "#target" double size
 // Hint: you can use .css, .width and .height
 
+// $(document).ready(function() {
+//   $('#target').css('width', '300px');
+//   $('#target').css('height', '200px');
+// })
+
 // EXERCISE: Empty Elements
-// Hint: you can use .empty
 
 // Remove all children and text of "#target"
+// Hint: you can use .empty
+
+// $(document).ready(function() {
+//   $('#target').empty();
+// })
 
 // EXERCISE: Delay
 
 // Show Alert with 1 second delay (Use "setTimeout")
 // Hint: you can use .setTimeout and alert
 
+
+
 // EXERCISE: Count
 
 // Show the number of children in an alert
 // Hint: you can use .children, .length and alert or you could use .find, .size and alert. the alert should show 5
 
+// $(document).ready(function() {
+//   alert ( "There are " + $('#target').children('.child').length + " children");
+// })
+
 // EXERCISE: Animate
 
 // Make "#target" double size with animation
 // Hint: you can use .animate, .width, and .height
+
+$(document).ready(function() {
+  $('#target').width(150).height(190).animate({
+    height: 400,
+    width: 300,
+  });
+})
 
 // EXERCISE: Alternate Color
 
